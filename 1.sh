577c22b2bcfcbe66d9dca67ea54f346542c38e22
bls4 <92715010+bls4@users.noreply.github.com>
2023-03-02 13:44:08 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,6 @@
 mkdir ~/.vnc;\
 apt update -y;\
-apt install -y tint2 tightvncserver autocutsel openbox lxterminal pcmanfm;\
+apt install -y tint2 tightvncserver autocutsel openbox lxterminal pcmanfm sudo;\
 echo "pw1971" | vncpasswd -f > ~/.vnc/passwd;\
 echo "#!/bin/bash" >> ~/.vnc/xstartup;\
 echo "xrdb $HOME/.Xresources" >> ~/.vnc/xstartup;\
@@ -10,7 +10,7 @@ echo "openbox-session "$@" &" >> ~/.vnc/xstartup;\
 echo "tint2 "$@" &" >> ~/.vnc/xstartup;\
 chmod +x ~/.vnc/passwd;\
 chmod +x ~/.vnc/xstartup;\
-sudo vncserver :0 -geometry 1600x900 -economictranslate -dontdisconnect "$@" & \
-wget https://github.com/pgaskin/easy-novnc/releases/download/v1.1.0/easy-novnc_linux-64bit;\
-chmod +x ./easy-noVnc_linux-64bit;\
+sudo vncserver :0 -geometry 1600x900 -economictranslate -dontdisconnect "$@" & ;\
+wget https://github.com/pgaskin/easy-novnc/releases/download/v1.1.0/easy-novnc_linux-64bit ;\
+chmod +x ./easy-noVnc_linux-64bit ;\
 ./easy-noVnc_linux-64bit --addr 0.0.0.0:6080 --port 5901
