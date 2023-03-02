cf2ea5bf4386d4140f062102e25b53bbee5ad2ab
bls4 <92715010+bls4@users.noreply.github.com>
2023-03-02 16:00:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,35 @@
-apt update;\
-apt install libgbm1 libnspr4 libnss3 libu2f-udev fonts-liberation -y;\
-apt-get install -y --no-install-recommends openbox tint2 xdg-utils lxterminal hsetroot tigervnc-standalone-server supervisor sudo dbus-x11 pcmanfm mousepad thonny python3-pip;\
-rm -rf /var/lib/apt/lists;\
-apt-get update -y;\
-apt-get install -y --no-install-recommends openssh-client wget curl rsync ca-certificates apulse libpulse0 tar xzip gzip bzip2 zip unzip;\
-rm -rf /var/lib/apt/lists;\
-cp menu.xml /etc/xdg/openbox/;\
-echo 'hsetroot -solid "#123456" &' >> /etc/xdg/openbox/autostart;\
-pip install jupyterlab selenium webdriver_manager undetected_chromedriver==3.4.4 translate;\
-pip3 install --upgrade requests;\
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;\
-sudo dpkg -i google-chrome-stable_current_amd64.deb;\
-rm -rf google-chrome-stable_current_amd64.deb;\
-apt --fix-broken install;\
-sed -i "s|Exec=/usr/bin/google-chrome-stable %U|Exec=/usr/bin/google-chrome-stable %U --no-sandbox --disable-dev-shm-usage --disable-gpu|g" /usr/share/applications/google-chrome.desktop;\
-mkdir /root/.jupyter;\
-echo "c.ServerApp.token = ''" > /root/.jupyter/jupyter_notebook_config.py;\
-mkdir -p /root/.config/tint2;\
-cp tint2rc /root/.config/tint2/;\
-cp supervisord.conf /etc/;\
-echo '/usr/bin/supervisord' >> /etc/xdg/openbox/autostart
+apt update -y ;\
+apt install -y openbox tigervnc-standalone-server supervisor ;\
+apt install -y hsetroot lxterminal nano wget xdg-utils wget tint2;\
+echo 'hsetroot -solid "#123456" &' >> /etc/xdg/openbox/autostart ;\
+echo '/usr/bin/supervisord' >> /etc/xdg/openbox/autostart ;\
+cp ./supervisord.conf /etc/ ;\
+cp ./easy-novnc /usr/local/bin/ ;\
+chmod +x /usr/local/bin/easy-novnc ;\
+cp menu.xml /etc/xdg/openbox/ ;\
+mkdir -p /root/.config/tint2 ;\
+cp tint2rc /root/.config/tint2/ ;\
+mkdir "/root/.vnc" ;\
+chown -R "root:root" "/root/.vnc" ;\
+echo "p1971w" > /root/.vnc/passwd  ;\
+chown -R "root:root" "/root/.vnc/passwd" ;\
+chmod 400 "/root/.vnc/passwd" ;\
+apt update ;\
+apt install python3-pip -y ;\
+pip install jupyterlab ;\
+pip install selenium ;\
+pip install webdriver_manager ;\
+pip install undetected_chromedriver==3.4.4 ;\
+pip install translate ;\
+pip3 install --upgrade requests ;\
+apt install libgbm1 libnspr4 libnss3 libu2f-udev fonts-liberation -y ;\
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ;\
+sudo dpkg -i google-chrome-stable_current_amd64.deb ;\
+rm -rf google-chrome-stable_current_amd64.deb ;\
+apt --fix-broken install ;\
+apt update ;\
+apt install dbus-x11 pcmanfm mousepad thonny -y ;\
+sed -i "s|Exec=/usr/bin/google-chrome-stable %U|Exec=/usr/bin/google-chrome-stable %U --no-sandbox --disable-dev-shm-usage --disable-gpu|g" /usr/share/applications/google-chrome.desktop ;\
+mkdir /root/.jupyter ;\
+echo "c.ServerApp.token = ''" > /root/.jupyter/jupyter_notebook_config.py ;\
+sudo ufw allow 8080
