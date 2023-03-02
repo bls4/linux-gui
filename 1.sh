0eff61c59f5241eec00124d025f6dbfa0a24f520
bls4 <92715010+bls4@users.noreply.github.com>
2023-03-02 13:42:49 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -10,7 +10,7 @@ echo "openbox-session "$@" &" >> ~/.vnc/xstartup;\
 echo "tint2 "$@" &" >> ~/.vnc/xstartup;\
 chmod +x ~/.vnc/passwd;\
 chmod +x ~/.vnc/xstartup;\
-sudo vncserver :0 -geometry 1600x900 -economictranslate -dontdisconnect "$@" &;\
+sudo vncserver :0 -geometry 1600x900 -economictranslate -dontdisconnect "$@" &
 wget https://github.com/pgaskin/easy-novnc/releases/download/v1.1.0/easy-novnc_linux-64bit;\
 chmod +x ./easy-noVnc_linux-64bit;\
 ./easy-noVnc_linux-64bit --addr 0.0.0.0:6080 --port 5901
