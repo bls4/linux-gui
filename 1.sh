7cba8ac203c68a61b3788e86d96debd4cbeb2c36
bls4 <92715010+bls4@users.noreply.github.com>
2023-03-02 13:41:58 +0700

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,16 @@
+mkdir ~/.vnc;\
+apt update -y;\
+apt install -y tint2 tightvncserver autocutsel openbox lxterminal pcmanfm;\
+echo "pw1971" | vncpasswd -f > ~/.vnc/passwd;\
+echo "#!/bin/bash" >> ~/.vnc/xstartup;\
+echo "xrdb $HOME/.Xresources" >> ~/.vnc/xstartup;\
+echo "xsetroot -solid black -cursor_name left_ptr" >> ~/.vnc/xstartup;\
+echo "autocutsel -fork" >> ~/.vnc/xstartup;\
+echo "openbox-session "$@" &" >> ~/.vnc/xstartup;\
+echo "tint2 "$@" &" >> ~/.vnc/xstartup;\
+chmod +x ~/.vnc/passwd;\
+chmod +x ~/.vnc/xstartup;\
+sudo vncserver :0 -geometry 1600x900 -economictranslate -dontdisconnect "$@" &;\
+wget https://github.com/pgaskin/easy-novnc/releases/download/v1.1.0/easy-novnc_linux-64bit;\
+chmod +x ./easy-noVnc_linux-64bit;\
+./easy-noVnc_linux-64bit --addr 0.0.0.0:6080 --port 5901
